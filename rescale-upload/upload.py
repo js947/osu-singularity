b0a0c986d4964293e0dbf61e6de3fec5fde86d95
Jeffrey Salmond <jeffrey@cambridgesupercomputing.com>
2020-05-19 19:10:08 +0100

    add output to uploader

--- rescale-upload/upload.py
+++ rescale-upload/upload.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 import os, sys
+import pprint
 import requests
 
 token = os.environ['INPUT_RESCALE_TOKEN']
 platform = os.environ['INPUT_RESCALE_PLATFORM']
 filename = sys.argv[1]
 
-requests.post(
-  'https://%s/api/v2/files/contents/' % platform,
-  headers={'Content-Type': 'multipart/form-data',
-           'Authorization': 'Token %s' % token},
-  files={'file': open(filename, 'rb')}
-)
+with open(filename, 'rb') as f:
+    r = requests.post(
+      'https://%s/api/v2/files/contents/' % platform,
+      headers={#'Content-Type': 'multipart/form-data',
+               'Authorization': 'Token %s' % token},
+      files={'file': f}
+    )
+
+print("status=", r.status_code)
+print("response")
+pprint.pprint(r.json())
